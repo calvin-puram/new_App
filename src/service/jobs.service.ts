38504aa3b4204a853173eaed01c407eef2b8dd1e
calvin <puram.calvin@gmail.com>
2020-09-02 16:43:08 +0100

    created the jobs class

---
+++ src/service/jobs.service.ts
@@ -0,0 +1,18 @@
+const baseUrl: string = "https://jobs.github.com/positions.json?page=1";
+import axios from "axios";
+
+class JobService {
+  public getJobs = async () => {
+    let totalPage = 1;
+    let page = 0;
+    const jobs = [];
+    while (totalPage > 0) {
+      const res = await axios.get(`${baseUrl}?page=${page}`);
+      const data = jobs.push(...res.data);
+      totalPage = res.data.length;
+      page++;
+    }
+  };
+}
+
+export default JobService;
