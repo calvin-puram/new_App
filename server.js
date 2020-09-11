5da252724383124ff5d86e4b2931ae7a77df52a6
calvin <puram.calvin@gmail.com>
2020-09-11 05:21:06 +0100

    added sessions to app

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const session = require("express-session");
+const passport = require("passport");
 
 const ConnectDB = require("./db");
 const router = require("./routes");
@@ -7,10 +9,25 @@ const router = require("./routes");
 dotenv.config({ path: "./.env" });
 
 const app = express();
-
 ConnectDB();
-
 app.use(morgan("dev"));
+
+const sess = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {},
+};
+
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+  sess.cookie.secure = true;
+}
+app.use(session(sess));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(router);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
